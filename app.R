fe71927abd1ae9ce177524c223003fd27cbceca1
HOUEDJI Rodrigue <rodhouedji@gmail.com>
2025-12-05 01:58:17 +0000

    Vérification des erreurs après la première partie du code

--- app.R
+++ app.R
@@ -9,4 +9,38 @@ library(ggplot2)
 library(bslib)
 
 #Premier morceau de code pour l'application
+thematic::thematic_shiny(font = "auto")
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+  theme = bs_theme(
+    version = 5
+  ),
+  # Application title
+  titlePanel("Starwars"),
+  h1("Star Wars Characters"),
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput(inputId = "taille",
+                  label = "Height of characters",
+                  min = 0,
+                  max = 250,
+                  value = 30),
+      actionButton(inputId = "boutton", label = "Cliquez-moi"),
+      selectInput(
+        inputId = "sexe",
+        label = "Choisissez le genre",
+        choices = c("masculine","feminine")
+      )
+    ),
+    # Show a plot of the generated distribution
+    mainPanel(
+      textOutput(outputId = "nbperso"),
+      plotOutput(outputId = "StarWarsPlot"),
+      DT::DTOutput(outputId = "tableau")
+    )
+    
+  )
+)
+
 
