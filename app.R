a42c0f5abbb5b09f708203e728754c614df034fb
HOUEDJI Rodrigue <rodhouedji@gmail.com>
2025-12-06 01:50:17 +0000

    Personnalisation de l'UI

--- app.R
+++ app.R
@@ -12,26 +12,47 @@ library(DT)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-  theme = bs_theme(bootswatch = "flatly"),
+  theme = bs_theme(
+    version = 5,
+    bootswatch = "minty"
+  ),
   
   # Application title
   titlePanel("Exploration des Diamants "),
-  # Sidebar with a slider input for number of bins 
+  # Sidebar  
   sidebarLayout(
     sidebarPanel(
+      
+      radioButtons(
+        inputId = "rose",
+        label = "Colorier les points en rose ?",
+        choices = c("Oui","Non"),
+        selected = "Oui"
+      ),
+      
+      selectInput(
+        inputId = "choix",
+        label = "Choisir une couleur à filtrer",
+        choices = c("D", "E", "F","G","H","I","J")
+      ),
+      
       sliderInput(
         inputId = "prix_max",
         label = "Prix maximum :",
         min = 300,
         max = 20000,
-        value = 5000)
-      
-    ),
+        value = 5000),
+    
+    actionButton(
+      inputId = "bouton",
+      label = "Visualisez le graph"
+    )
+  ),
     
     # Show a plot of the generated distribution
     mainPanel(
-      plotlyOutput("distPlot"),
-      DTOutput("table")
+      plotlyOutput(outputId = "distPlot"),
+      DTOutput(outputId = "table")
     )
   )
 )
@@ -39,12 +60,16 @@ ui <- fluidPage(
 # server 
 server <- function(input, output) {
   
+  #graphique plotly
   output$distPlot <- renderPlotly({
     d <- diamonds %>%
-      filter(price <= input$prix_max)
+      filter(price <= input$prix_max,
+             color == input$choix)
+    
+    col <- ifelse(input$rose == "Oui", "pink", "steelblue") 
     
     pty<-ggplot(d, aes(x = carat, y = price)) +
-      geom_point(alpha = 0.3) +
+      geom_point(color=col,alpha = 0.3) +
       labs(
         x = "Carat",
         y = "Price",
@@ -53,8 +78,16 @@ server <- function(input, output) {
       theme_minimal()
     ggplotly(pty)
   })
-  
+  # Tableau interactif 
+  output$table <- renderDT({
+    
+    d <- diamonds %>%
+      filter(price <= input$prix_max)
+    
+    datatable(d)
+  })
 
+  
 }
 
 # Run the application 
